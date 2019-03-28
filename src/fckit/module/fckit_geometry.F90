b2f44938a063562f1e730a9f871a6ed71df955d4
benjaminmenetrier <benjamin.menetrier@irit.fr>
2019-03-28 23:18:19 +0100

    Interfaces for KDTree

--- src/fckit/module/fckit_geometry.F90
+++ src/fckit/module/fckit_geometry.F90
@@ -12,12 +12,13 @@ module fckit_geometry_module
 implicit none
 private
 
-public :: unit_sphere_distance, unit_sphere_lonlat2xyz, unit_sphere_xyz2lonlat
+public :: sphere_distance, sphere_lonlat2xyz, sphere_xyz2lonlat
+public :: kdtree_create, kdtree_destroy, kdtree_k_nearest_neighbors, kdtree_find_in_sphere
 
 !------------------------------------------------------------------------------
 interface
-  ! double fckit__unit_sphere_distance ( const double & lonA, const double & latA, const double & lonB, const double & latB)
-  function fckit__unit_sphere_distance( lonA, latA, lonB, latB ) result(distance) bind(C)
+  ! double fckit__sphere_distance(const double & lonA, const double & latA, const double & lonB, const double & latB)
+  function fckit__sphere_distance(lonA, latA, lonB, latB) result(distance) bind(C)
     use iso_c_binding, only: c_double
     real(c_double) :: distance
     real(c_double) :: lonA
@@ -26,8 +27,8 @@ interface
     real(c_double) :: latB
   end function
 
-  ! void fckit__unit_sphere_lonlat2xyz ( const double & lon, const double & lat, double & x, double & y, double & a)
-  subroutine fckit__unit_sphere_lonlat2xyz( lon, lat, x, y, z ) bind(C)
+  ! void fckit__sphere_lonlat2xyz(const double & lon, const double & lat, double & x, double & y, double & a)
+  subroutine fckit__sphere_lonlat2xyz(lon, lat, x, y, z) bind(C)
     use iso_c_binding, only: c_double
     real(c_double) :: lon
     real(c_double) :: lat
@@ -36,14 +37,49 @@ interface
     real(c_double) :: z
   end subroutine
 
-  ! void fckit__unit_sphere_xyz2lonlat ( const double & x, const double & y, const double & z, double & lon, double & lat)
-  subroutine fckit__unit_sphere_xyz2lonlat( x, y, z, lon, lat ) bind(C)
+  ! void fckit__sphere_xyz2lonlat(const double & x, const double & y, const double & z, double & lon, double & lat)
+  subroutine fckit__sphere_xyz2lonlat(x, y, z, lon, lat) bind(C)
     use iso_c_binding, only: c_double
     real(c_double) :: x
     real(c_double) :: y
     real(c_double) :: z
     real(c_double) :: lon
     real(c_double) :: lat
+  end subroutine
+
+  ! eckit::KDTreeMemory<TreeTrait> fckit__kdtree_create(int n, double lon[], double lat[]) {
+  function fckit__kdtree_create(n, lon, lat) result(kd) bind(C)
+    use iso_c_binding, only: c_ptr, c_int, c_double
+    type(c_ptr) :: kd
+    integer(c_int) :: n
+    real(c_double) :: lon(n)
+    real(c_double) :: lat(n)
+  end function
+
+  ! void fckit__kdtree_destroy(eckit::KDTreeMemory<TreeTrait> * kd) {
+  subroutine fckit__kdtree_destroy(kd) bind(C)
+    use iso_c_binding, only: c_ptr
+    type(c_ptr),value :: kd
+  end subroutine
+
+  ! void fckit__kdtree_k_nearest_neighbors(eckit::KDTreeMemory<TreeTrait> * kd, const double & lon, const double & lat, const int & nn, int * nn_index) {
+  subroutine fckit__kdtree_k_nearest_neighbors(kd, lon, lat, nn, nn_index) bind(C)
+    use iso_c_binding, only: c_ptr, c_int, c_double
+    type(c_ptr),value :: kd
+    real(c_double) :: lon
+    real(c_double) :: lat
+    integer(c_int) :: nn
+    integer(c_int) :: nn_index(nn)
+  end subroutine
+
+  ! void fckit__kdtree_find_in_sphere(eckit::KDTreeMemory<TreeTrait> * kd, const double & lon, const double & lat, const double & r, int & nn) {
+  subroutine fckit__kdtree_find_in_sphere(kd, lon, lat, r, nn) bind(C)
+    use iso_c_binding, only: c_ptr, c_int, c_double
+    type(c_ptr),value :: kd
+    real(c_double) :: lon
+    real(c_double) :: lat
+    real(c_double) :: r
+    integer(c_int) :: nn
   end subroutine
 end interface
 
@@ -51,34 +87,70 @@ end interface
 contains
 ! =============================================================================
 
-function unit_sphere_distance(lonA, latA, lonB, latB) result(distance)
+function sphere_distance(lonA, latA, lonB, latB) result(distance)
   use iso_c_binding, only: c_double
   real(c_double) :: distance
   real(c_double), intent(in) :: lonA
   real(c_double), intent(in) :: latA
   real(c_double), intent(in) :: lonB
   real(c_double), intent(in) :: latB
-  distance = fckit__unit_sphere_distance(lonA, latA, lonB, latB)
+  distance = fckit__sphere_distance(lonA, latA, lonB, latB)
 end function
 
-subroutine unit_sphere_lonlat2xyz(lon, lat, x, y, z)
+subroutine sphere_lonlat2xyz(lon, lat, x, y, z)
   use iso_c_binding, only: c_double
   real(c_double), intent(in) :: lon
   real(c_double), intent(in) :: lat
   real(c_double), intent(out) :: x
   real(c_double), intent(out) :: y
   real(c_double), intent(out) :: z
-  call fckit__unit_sphere_lonlat2xyz(lon, lat, x, y, z)
+  call fckit__sphere_lonlat2xyz(lon, lat, x, y, z)
 end subroutine
 
-subroutine unit_sphere_xyz2lonlat(x, y, z, lon, lat)
+subroutine sphere_xyz2lonlat(x, y, z, lon, lat)
   use iso_c_binding, only: c_double
   real(c_double), intent(in) :: x
   real(c_double), intent(in) :: y
   real(c_double), intent(in) :: z
   real(c_double), intent(out) :: lon
   real(c_double), intent(out) :: lat
-  call fckit__unit_sphere_xyz2lonlat(x, y, z, lon, lat)
+  call fckit__sphere_xyz2lonlat(x, y, z, lon, lat)
+end subroutine
+
+function kdtree_create(n, lon, lat) result(kdtree)
+  use iso_c_binding, only: c_ptr, c_int, c_double
+  type(c_ptr) :: kdtree
+  integer(c_int), intent(in) :: n
+  real(c_double), intent(in) :: lon(n)
+  real(c_double), intent(in) :: lat(n)
+  kdtree = fckit__kdtree_create(n, lon, lat)
+end function
+
+subroutine kdtree_destroy(kd)
+  use iso_c_binding, only: c_ptr
+  type(c_ptr),intent(inout) :: kd
+  call fckit__kdtree_destroy(kd)
+end subroutine
+
+subroutine kdtree_k_nearest_neighbors(kd, lon, lat, nn, nn_index)
+  use iso_c_binding, only: c_ptr, c_int, c_double
+  type(c_ptr),intent(in) :: kd
+  real(c_double),intent(in) :: lon
+  real(c_double),intent(in) :: lat
+  integer(c_int),intent(in) :: nn
+  integer(c_int),intent(out) :: nn_index(nn)
+  call fckit__kdtree_k_nearest_neighbors(kd, lon, lat, nn, nn_index)
+  nn_index = nn_index+1
+end subroutine
+
+subroutine kdtree_find_in_sphere(kd, lon, lat, r, nn)
+  use iso_c_binding, only: c_ptr, c_int, c_double
+  type(c_ptr),intent(in) :: kd
+  real(c_double),intent(in) :: lon
+  real(c_double),intent(in) :: lat
+  real(c_double),intent(in) :: r
+  integer(c_int),intent(out) :: nn
+  call fckit__kdtree_find_in_sphere(kd, lon, lat, r, nn)
 end subroutine
 
 end module fckit_geometry_module
--- src/fckit/module/fckit_geometry.cc
+++ src/fckit/module/fckit_geometry.cc
@@ -8,6 +8,10 @@
  * does it submit to any jurisdiction.
  */
 
+#include <cmath>
+#include <list>
+
+#include "eckit/container/KDTree.h"
 #include "eckit/geometry/Point2.h"
 #include "eckit/geometry/Point3.h"
 #include "eckit/geometry/UnitSphere.h"
@@ -16,13 +20,13 @@ namespace fckit {
 
 extern "C" {
 
-double fckit__unit_sphere_distance ( const double & lonA, const double & latA, const double & lonB, const double & latB) {
+double fckit__sphere_distance(const double & lonA, const double & latA, const double & lonB, const double & latB) {
   const eckit::geometry::Point2 A(lonA, latA);
   const eckit::geometry::Point2 B(lonB, latB);
   return eckit::geometry::UnitSphere::distance(A, B);
 }
 
-void fckit__unit_sphere_lonlat2xyz ( const double & lon, const double & lat, double & x, double & y, double & z) {
+void fckit__sphere_lonlat2xyz(const double & lon, const double & lat, double & x, double & y, double & z) {
   const eckit::geometry::Point2 lonlat(lon, lat);
   eckit::geometry::Point3 xyz;
   eckit::geometry::UnitSphere::convertSphericalToCartesian(lonlat, xyz);
@@ -31,12 +35,79 @@ void fckit__unit_sphere_lonlat2xyz ( const double & lon, const double & lat, dou
   z = xyz[2];
 }
 
-void fckit__unit_sphere_xyz2lonlat ( const double & x, const double & y, const double & z, double & lon, double & lat) {
+void fckit__sphere_xyz2lonlat(const double & x, const double & y, const double & z, double & lon, double & lat) {
   const eckit::geometry::Point3 xyz(x, y, z);
   eckit::geometry::Point2 lonlat;
   eckit::geometry::UnitSphere::convertCartesianToSpherical(xyz, lonlat);
   lon = lonlat[0];
   lat = lonlat[1];
+}
+
+struct TreeTrait {
+    typedef eckit::geometry::Point3 Point;
+    typedef double                  Payload;
+};
+
+typedef eckit::KDTreeMemory<TreeTrait> KDTree;
+
+KDTree* fckit__kdtree_create(const int & n, const double * lon, const double * lat) {
+    // Define tree
+    KDTree * kd = new KDTree();
+
+    // Define points list
+    typedef KDTree::PointType Point;
+    std::vector<KDTree::Value> points;
+    for (unsigned int i = 0; i < n; i++) {
+        eckit::geometry::Point2 lonlat(lon[i], lat[i]);
+        Point xyz = Point();
+        eckit::geometry::UnitSphere::convertSphericalToCartesian(lonlat, xyz);
+        double index = static_cast<double>(i);
+        KDTree::Value v(xyz, index);
+        points.push_back(v);
+    }
+
+    // Build and return KDTree
+    kd->build(points.begin(), points.end());
+    return kd;
+}
+
+void fckit__kdtree_destroy (KDTree * kd) {
+    delete kd;
+}
+
+void fckit__kdtree_k_nearest_neighbors(KDTree * kd, const double & lon, const double & lat, const int & nn, int * nn_index) {
+    // Define central point
+    eckit::geometry::Point2 lonlat(lon, lat);
+    eckit::geometry::Point3 xyz;
+    eckit::geometry::UnitSphere::convertSphericalToCartesian(lonlat, xyz);
+
+    // Find nn nearest neighbors
+    KDTree::NodeList list = kd->kNearestNeighbours(xyz, nn);
+
+    // Copy nearest neighbors index
+    int i = 0;
+    for (KDTree::NodeList::iterator it = list.begin(); it != list.end(); ++it)
+    {
+       nn_index[i] = static_cast<int>(it->payload());
+       i++;
+    }
+    ASSERT(i==nn);
+}
+
+void fckit__kdtree_find_in_sphere(KDTree * kd, const double & lon, const double & lat, const double & r, int & nn) {
+    // Define central point
+    eckit::geometry::Point2 lonlat(lon, lat);
+    eckit::geometry::Point3 xyz;
+    eckit::geometry::UnitSphere::convertSphericalToCartesian(lonlat, xyz);
+
+    // Convert radius on unit sphere to chord
+    double chord = 2.0*sin(0.5*std::min(r, M_PI));
+
+    // Find nearest neighbors within radius r
+    KDTree::NodeList list = kd->findInSphere(xyz, chord);
+
+    // Copy number of neighbors
+    nn = list.size();
 }
 
 } // extern "C"
